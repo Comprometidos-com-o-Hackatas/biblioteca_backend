be9d872524d74deb1805bd96681af9173595387b
jaotarzan <joaovssouza59@gmail.com>
2024-07-15 15:01:21 -0300

    feat: cretion of app user and organization of apps in core folder

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'biblioteca',
+    'core.biblioteca',
+    'core.usuario',
     'rest_framework'
 ]
 
--- django_project/urls.py
+++ django_project/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from biblioteca.views import CategoriaViewSet, GeneroViewSet, AutoresViewSet
+from core.biblioteca.views import CategoriaViewSet, GeneroViewSet, AutoresViewSet
 
 router = DefaultRouter()
 router.register(r'generos', GeneroViewSet, basename='generos')
