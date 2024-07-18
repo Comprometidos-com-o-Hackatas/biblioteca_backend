f51b26cbd17a4a37de7d9ef9b0a8d6b864cce349
jaotarzan <joaovssouza59@gmail.com>
2024-07-18 13:52:33 -0300

    feat: criação e configuração de usuário

--- core/usuario/views.py
+++ core/usuario/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Usuario
+from .serializers import UsuarioSerializer
+
+
+class UsuarioViewSet(ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
--- django_project/urls.py
+++ django_project/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 from rest_framework.routers import DefaultRouter
+
 from core.biblioteca.views import GeneroViewSet, AutoresViewSet
+from core.usuario.router import router as usuarioRouter
 
 router = DefaultRouter()
 router.register(r'generos', GeneroViewSet, basename='generos')
@@ -25,5 +28,6 @@ router.register(r'autores', AutoresViewSet, basename='autores')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/usuarios/', include(usuarioRouter.urls)),
 ]
