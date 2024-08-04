f9c231fa882f72573dec6e8bb7fb14a53300bfb5
luansilva25 <luansilva250807@gmail.com>
2024-08-04 13:59:10 -0300

    FEAT: create model, serializer, views, URL of 'livroPego' #14

--- core/biblioteca/views/__init__.py
+++ core/biblioteca/views/__init__.py
@@ -2,3 +2,4 @@ from .categoria import CategoriaViewSet
 from .genero import GeneroViewSet
 from .autores import AutoresViewSet
 from .livro import LivroViewSet
+from .livroPego import LivroPegoViewSet
--- django_project/urls.py
+++ django_project/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
-from core.biblioteca.views import CategoriaViewSet, GeneroViewSet, AutoresViewSet, LivroViewSet
+from core.biblioteca.views import CategoriaViewSet, GeneroViewSet, AutoresViewSet, LivroViewSet, LivroPegoViewSet
 from core.usuario.router import router as usuarioRouter
 from core.uploader.router import router as uploaderRouter
 
@@ -35,6 +35,7 @@ router.register(r'generos', GeneroViewSet, basename='generos')
 router.register(r'autores', AutoresViewSet, basename='autores')
 router.register(r'categorias', CategoriaViewSet)
 router.register(r'livro', LivroViewSet)
+router.register(r'livropego', LivroPegoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
