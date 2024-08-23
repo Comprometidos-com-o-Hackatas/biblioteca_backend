02eecbb23c0a74d9bd9f42cecdcf78ca35bef08b
Jaotarzan <Jaotarzan>
2024-08-23 11:26:03 -0300

    FIX: Removing duplicates

--- core/usuario/migrations/0003_alter_usuario_passage_id.py
+++ core/usuario/migrations/0003_alter_usuario_passage_id.py
@@ -12,7 +12,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='usuario',
             name='passage_id',
-            field=models.CharField(default=uuid.UUID('4a3e1dbb-31ec-4d29-8c8c-4bdad05002e4'), max_length=255, unique=True),
             field=models.CharField(default=uuid.UUID('f2ff1d20-ecd0-4797-9e96-c90efb8296b1'), max_length=255, unique=True),
         ),
     ]
