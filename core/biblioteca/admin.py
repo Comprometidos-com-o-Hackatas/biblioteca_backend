9ce97711053f6710b5cd1f0ffe925f597fd01e42
Gabz047 <gabe1612mail@gmail.com>
2024-08-31 17:23:43 -0300

    Added More fields to livro serializer

--- core/biblioteca/admin.py
+++ core/biblioteca/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Autores)
-admin.site.register(models.Favorito)
+admin.site.register(models.Categoria)
 admin.site.register(models.Livro)
 admin.site.register(models.Avaliacao)
 admin.site.register(models.Generos)
