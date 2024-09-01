2f79207998eb78646bd0e1ee582780af42dfcd92
Gabz047 <gabe1612mail@gmail.com>
2024-08-31 21:28:13 -0300

    Added nota to livro serializer fields

--- core/biblioteca/serializers/livro.py
+++ core/biblioteca/serializers/livro.py
@@ -14,7 +14,7 @@ class LivroListSerializer(ModelSerializer):
     capa = ImageSerializer(required=False, read_only=True)
     class Meta:
         model = Livro
-        fields = ['id', 'titulo', 'autores', 'capa', 'capa_attachment_key', 'disponivel', 'categoria', 'generos', 'descricao']
+        fields = ['id', 'titulo', 'autores', 'capa', 'capa_attachment_key', 'disponivel', 'categoria', 'generos', 'descricao', 'nota']
         depth = 1
 
 class LivroDetailSerializer(ModelSerializer):
