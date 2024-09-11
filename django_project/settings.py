a4a1a333c3c21fa27a5d86052236736d103c07b5
luansilva25 <luansilva250807@gmail.com>
2024-09-11 13:27:43 -0300

    deploy configs

--- django_project/settings.py
+++ django_project/settings.py
@@ -130,26 +130,25 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+""""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-""""
+"""""""""
 DATABASES = {
     'default': {
         'ENGINE': "django.db.backends.postgresql_psycopg2",
         'HOST': "aws-0-us-west-1.pooler.supabase.com",
         'NAME': "postgres",
         'PORT': "6543",
-        "USER": "postgres.evupsoqmhislqycjcgpf",
-        "PASSWORD": "luan250807@#"
+        "USER": "postgres.bhuzqvriqaggtmhzssrm",
+        "PASSWORD": "comprometidos2024"
     }
 }
-"""""""""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
