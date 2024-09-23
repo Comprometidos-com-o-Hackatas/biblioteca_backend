a9054a1d4777ed4110790949340417d4c34d5094
luansilva25 <luansilva250807@gmail.com>
2024-09-23 12:49:25 -0300

    pagination removed

--- django_project/settings.py
+++ django_project/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'rest_framework_simplejwt',
     "corsheaders",
+    'django_extensions',
     'core.biblioteca',
     'core.usuario',
     'core.uploader',
@@ -220,8 +221,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
 }
 
 SPECTACULAR_SETTINGS = {
