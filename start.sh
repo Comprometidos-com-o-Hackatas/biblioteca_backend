2b0989b9363fde762345c221f806569ba3337b41
Gabz047 <gabe1612mail@gmail.com>
2025-04-30 21:09:58 -0300

    REFACTOR: changed dockerfile

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "📦 Rodando makemigrations..."
+pdm run python manage.py makemigrations
+
+echo "📂 Rodando migrate..."
+pdm run python manage.py migrate
+
+echo "🚀 Iniciando Gunicorn..."
+pdm run gunicorn django_project.wsgi:application --bind 0.0.0.0:8001
